5d2e8d88eedca97db7d7841f4b5b98d3ec2f26af
SavannahBolger <C00215747@itcarlow.ie>
2020-01-06 11:09:44 +0000

    update

--- records/hello.c
+++ records/hello.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lobby_system.erl"
+#include "player.c"
 
 int main()
 {
-    lobby_system:lobby();
+    char A1 = "A1";
+	char A2 = "A2";
+    char A3 = "A2";
+    player P1 = {"Joe", "KillerJoe", 100, A1};
+	player P2 = {"Jane", "KillerJane", 100, A2};
+    player P3 = {"John", "KillerJohn", 100, A2};
+
+    if(P1.achievements == P2.achievements)
+    {
+        printf("p1 = p2");
+    }
+    else if(P1.achievements == P3.achievements)
+    {
+        printf("p1 = p3");
+    }
+    else if(P3.achievements == P2.achievements)
+    {
+        printf("p3 = p2");
+    }
+    else
+    {
+        printf("no matches");
+    }
+    
+    //lobby_system:lobby();
     return 0;
 }
---
+++ records/player.c
@@ -0,0 +1,7 @@
+typedef struct Player
+{
+    char name[50];
+    char gamer_tag[50]; 
+    int points;
+    char achievements[50];
+} player;
